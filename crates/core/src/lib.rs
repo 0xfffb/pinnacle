4621a392ce25631c0db2d34cce867176bf0afb49
0xfffb <jailbreak@aliyun.com>
2026-09-22 20:52:49 +0800

    Refactor response handling by renaming module and removing deprecated file
    - Renamed the `response` module to `respond` for clarity.
    - Removed the old `response.rs` file, streamlining the codebase and improving maintainability.

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,11 +1,11 @@
 mod middleware;
-mod response;
+mod respond;
 mod stack;
 mod types;
 
 pub use bytes::Bytes;
 pub use http::{Request, Response, StatusCode};
 pub use middleware::{from_fn_with_state, FromFnLayer, Next};
-pub use response::Respond;
+pub use respond::Respond;
 pub use stack::{Stack, StackBuilder};
 pub use types::{ClientIp, Decision};
